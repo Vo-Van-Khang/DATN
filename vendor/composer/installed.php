636f3db8af692315f5f043da66b028e16835e5bb
Vo-Van-Khang <vovankhang11102004@gmail.com>
2024-10-20 12:11:39 +0700

    repair source

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v11.2.0',
-        'version' => '11.2.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1cee0fc6fa2fd50a9ca2899567a572742890da55',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -386,9 +386,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v11.2.0',
-            'version' => '11.2.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1cee0fc6fa2fd50a9ca2899567a572742890da55',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
